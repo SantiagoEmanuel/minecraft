bca6a55a239547e185851dd5c1a35324db86d2ce
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-08-19 18:56:52 -0300

    Remove import env variables and console.log

--- client/src/utils/serverStatus.js
+++ client/src/utils/serverStatus.js
@@ -1,11 +1,9 @@
 export async function serverStatus() {
-  const IP = import.meta.env.VITE_SERVER_IP
-  const PORT = import.meta.env.VITE_SERVER_PORT
-
-  return await fetch(`https://mcapi.us/server/status?ip=${IP}&port=${PORT}`)
+  return await fetch(
+    `https://mcapi.us/server/status?ip=172.93.110.127&port=7076`,
+  )
     .then((r) => r.json())
     .then((response) => {
-      console.log(response)
       return response
     })
 }
