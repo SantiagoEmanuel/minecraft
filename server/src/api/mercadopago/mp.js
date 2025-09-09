393102c715cac1d609024f7991f19f32b5089dd7
Santiago Emanuel Mustafá Font <santiagomustafa3@gmail.com>
2025-09-09 10:48:31 -0300

    Refactor MercadoPago configuration to export client instance directly

--- server/src/api/mercadopago/mp.js
+++ server/src/api/mercadopago/mp.js
@@ -2,9 +2,9 @@ import { MercadoPagoConfig, Preference, Payment } from "mercadopago";
 import dotenv from "dotenv";
 dotenv.config();
 
-const config = new MercadoPagoConfig({
+export const client = new MercadoPagoConfig({
   accessToken: process.env.MP_ACCESS_TOKEN,
 });
 
-export const preference = new Preference(config);
-export const payment = new Payment(config);
+export const preference = new Preference(client);
+export const payment = new Payment(client);
