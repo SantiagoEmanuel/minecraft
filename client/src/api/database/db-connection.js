f5c34e850750a73dcbc046ce9711a1174be90d17
Santiago Emanuel Mustafá Font <santiagomustafa3@gmail.com>
2025-09-09 10:57:06 -0300

    Update db-connection to use environment-based baseURL for development and production

--- client/src/api/database/db-connection.js
+++ client/src/api/database/db-connection.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
 // const baseURL = 'https://minecraft-nnsl.onrender.com'
-const baseURL = 'http://localhost:8080'
+const baseURL =
+  import.meta.env.MODE === 'dev'
+    ? 'http://localhost:8080'
+    : 'https://minecraft-server-lt5y.onrender.com'
 
 export const fetch = axios.create({
   baseURL: baseURL,
